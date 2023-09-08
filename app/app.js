a2e97a4a59434c984390fe74a047f41bd17032b0
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2023-09-08 12:54:19 -0600

    added comment to help run server
    added comment to help run server from computer

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 
 const socket = io("ws://96.51.136.132:3000");
+//use ws://localhost:3000 if running locally for testing purposes
 
 socket.on('message', text => {
     const el = document.createElement('li');
