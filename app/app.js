329e3966d1ea5defc80d5d9ed0c923a0f6113183
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2023-09-08 14:06:43 -0600

    minor css added
    minor css added. works fine in full screen will need to adjust

--- app/app.js
+++ app/app.js
@@ -1,11 +1,12 @@
 
-const socket = io("ws://96.51.136.132:3000");
-//use ws://localhost:3000 if running locally for testing purposes
+//const socket = io("ws://96.51.136.132:3000");  // When hosting on server (not local)
+const socket = io("ws://localhost:3000");      // When hosting locally
 
 socket.on('message', text => {
     const el = document.createElement('li');
     el.innerHTML = text;
     document.querySelector('ul').appendChild(el);
+    scrollToBottom();
 });
 
 document.querySelector('button').addEventListener('click', () => {
@@ -13,3 +14,11 @@ document.querySelector('button').addEventListener('click', () => {
     socket.emit('message', text);
     document.querySelector('input').value = '';
 });
+
+
+// testing 
+// Function to scroll to the bottom of the <ul>
+function scrollToBottom() {
+    const messageList = document.querySelector('ul');
+    messageList.scrollTop = messageList.scrollHeight;
+}
