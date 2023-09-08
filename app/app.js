5c8cad405a2db0ce89775f31b1b8f57684754395
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2023-09-07 21:46:14 -0600

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,14 @@
+
+const socket = io("ws://localhost:3000");
+
+socket.on('message', text => {
+    const el = document.createElement('li');
+    el.innerHTML = text;
+    document.querySelector('ul').appendChild(el);
+});
+
+document.querySelector('button').addEventListener('click', () => {
+    const text = document.querySelector('input').value;
+    socket.emit('message', text);
+    document.querySelector('input').value = '';
+});
