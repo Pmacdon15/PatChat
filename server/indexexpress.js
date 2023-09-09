0c30d6a48d1e972b2ef08d14e7967b82be83fc44
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2023-09-08 20:01:14 -0600

    change port
    changed port

--- server/indexexpress.js
+++ server/indexexpress.js
@@ -82,7 +82,7 @@ io.on('connection', (socket) => {
 //     });
 // });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
